c07ee035453148dd675c4cc08ca8109281d13d70
esteranodin <esteranodin@hotmail.fr>
2026-02-18 12:09:14 +0100

    MAJ: unicodes guillemets anglais

--- utils/textFormatting.ts
+++ utils/textFormatting.ts
@@ -28,6 +28,8 @@ function decodeHtmlEntities(text: string): string {
     .replace(/&#8212;/g, "—")
     .replace(/&#171;/g, "«")
     .replace(/&#187;/g, "»")
+    .replace(/&#8220;/g, "«")
+    .replace(/&#8221;/g, "»")
     .replace(/&laquo;/g, "«")
     .replace(/&raquo;/g, "»");
 }
