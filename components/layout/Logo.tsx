abf07b1870e35f54809ea912ef0f2514c95a6b81
esteranodin <esteranodin@hotmail.fr>
2026-03-30 20:49:43 +0200

    MAJ: reponsive logo

--- components/layout/Logo.tsx
+++ components/layout/Logo.tsx
@@ -4,7 +4,7 @@ interface LogoProps {
 
 export function Logo({ className }: LogoProps) {
   return (
-    <picture className="block w-full max-[768px]:w-49">
+    <picture className="block w-full max-[769px]:w-49">
       <source srcSet="/LogoRS.png" media="(max-width: 768px)" />
       <source
         srcSet="/Logo-fd-blanc.png"
